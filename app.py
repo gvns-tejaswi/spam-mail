720397e7d5ff519b8c6c9d44a45d24af84e11a20
gvns-tejaswi <gvns0221@gmail.com>
2026-04-07 16:35:59 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template
+import joblib
+
+model = joblib.load('spam_detection_model.pkl')
+vectorizer = joblib.load('count_vectorizer.pkl')
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict')
+def predict():
+    text = request.args.get('text')
+    data = vectorizer.transform([text])
+    result = model.predict(data)
+
+    if result[0] == 1:
+        return "Spam"
+    else:
+        return "Not Spam"
+
+app.run(host='0.0.0.0', port=5000)
